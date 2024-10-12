9ccc4d90b0c6af76e3c12627f076c81d2c8d7668
abirramy <ramyaabiramiv@gmail.com>
2024-10-13 02:13:11 +0530

    NEW FILES

---
+++ src/OCT_SDET/OCT_SDET_0110/Lab0110.java
@@ -0,0 +1,11 @@
+package OCT_SDET.OCT_SDET_0110;
+
+public class Lab0110 {
+    //SAMPLE
+    public static void main (String args []){
+        System.out.println("Hello world!");
+        /*USED FOR MULTIPLE
+        LINES COMMENT*/
+
+    }
+}
